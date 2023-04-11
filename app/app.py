7a0d4aaa00bbfb4a9ae92a6e0ceadd3fe9f93676
Boxuan <visionarybottud@gmail.com>
2023-04-11 11:11:06 +0200

    generate ten adaptive quiz questions

--- app/app.py
+++ app/app.py
@@ -1303,6 +1303,32 @@ def select_quiz():
     return render_template('quizs_select.html', quizs=quiz_rows)
 
 
+@app.route('/generate_quiz/<string:user_id>', methods=('GET', 'POST'))
+def generate_quiz(user_id):
+    conn = get_db_connection()
+    c = conn.cursor()
+
+    c.execute('SELECT question_id_right_answer, question_id_wrong_answer FROM users WHERE username = ?', (user_id,))
+    result = c.fetchone()
+    correct_questions = result[0].split(',') if result[0] else []
+    incorrect_questions = result[1].split(',') if result[1] else []
+
+    # exclude the right questions
+    c.execute('SELECT question_id FROM questions WHERE question_id NOT IN ({}) ORDER BY RANDOM() LIMIT 8'.format(','.join('?' for _ in correct_questions)), correct_questions)
+    question_ids = [row[0] for row in c.fetchall()]
+
+    # choose two questions from wrong questions poll
+    if len(incorrect_questions) >= 2:
+        incorrect_questions = random.sample(incorrect_questions, 2)
+    else:
+        incorrect_questions = random.sample(incorrect_questions, len(incorrect_questions))
+
+    # combine ten questions and return
+    selected_question_ids = question_ids + incorrect_questions
+    random.shuffle(selected_question_ids)
+    return ",".join(str(x) for x in selected_question_ids)
+
+
 
 @login_manager.user_loader
 def load_user(id):
