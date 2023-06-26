aad78a5e4b41200b7826a54a551e6819ecd3f109
Christoph Reif <christoph.reif@smartstudy.at>
2023-06-26 10:27:44 +0200

    Refactor quiz first step

--- chatbot/actions/quiz.py
+++ chatbot/actions/quiz.py
@@ -3,56 +3,164 @@ from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, EventType
-from rasa_sdk.forms import FormAction
 from utils.utils import *
 from utils.db import *
 from utils.quiz import *
 
-# This form is to launch quiz for a user
-class QuizForm(Action):
+
+# This class will ask the user to select the quiz that he/she wants to take for form quiz_info_form
+class QuizNumber(Action):
+
     def name(self) -> Text:
-        return "action_quiz_form"
-    
-    def submit(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-    ) -> List[Dict]:
-        """Define what the form has to do
-            after all required slots are filled"""
-        # utter submit template
-        quiz_course = tracker.get_slot('quiz_course')
-        quiz_number = tracker.get_slot('quiz_number')
-        quiz_over = tracker.get_slot('quiz_over')
-        answer = tracker.get_slot('answer')
-        logger.info(f"[{tracker.sender_id}] {__file__} :  Inside quiz_info_form submit function called ")
-        logger.info(f"[{tracker.sender_id}] {__file__} :  {quiz_course} {quiz_number} {quiz_over} {answer}")
-        # dispatcher.utter_message("Thank you for providing the required information!")
-        return []
-    
+        return "action_ask_quiz_number"
     def run(
         self, 
-        dispatcher: CollectingDispatcher, 
-        tracker: Tracker, 
-        domain: Dict
-    ) -> List[EventType]:
-        # logger.info(f"[{tracker.sender_id}] {__file__} {inspect.currentframe().f_code.co_name}")
-        required_slots = ["quiz_number", "quiz_over"]
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
 
-        for slot_name in required_slots:
-            if tracker.slots.get(slot_name) is None:
-                if slot_name == 'quiz_number':
-                    tell = "Please enter the quiz number that you want to launch ?"
-                    dispatcher.utter_message(text=tell)
-                elif slot_name == 'quiz_over':
-                    tell = "Please enter the quiz_over that you want to launch ?"
-                    dispatcher.utter_message(text=tell)
-                return [SlotSet("requested_slot", slot_name)]
+        course_id = current_course_id(tracker)
+        listofQuiz = getListofQuiz(course_id)
+        if len(listofQuiz) == 0:
+            ask = f"There is no quiz for course {tracker.get_slot('quiz_course')}"
+            dispatcher.utter_message(text=ask)
+        else:
+            ask = f"Please enter the quiz number from {listofQuiz}"
+            ask = f"Please note that your answers must be separated by a coma"
+            buttons = quiz_buttons(listofQuiz)
+            buttons = button_it(buttons)
+            dispatcher.utter_message(text=ask, buttons=buttons)
+        return []
 
-            dispatcher.utter_message(text="H: " + str(tracker.slots.get(slot_name)) + " slot_name " + slot_name)
+# action_ask_<slot>
+# This class will ask the quiz question to the user for form quiz_info_form
+class QuizOver(Action):
+    def name(self) -> Text:
+        return "action_ask_quiz_over"
 
-        return [SlotSet("requested_slot", None)]
+    def run(self, 
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        
+        # get question number that needs to be asked
+        quiz_question_count = tracker.get_slot('quiz_question_count')
+        if quiz_question_count is None:
+            quiz_question_count = 0
+       
+        # fetch the question from the list of question
+        quiz_question_list = tracker.get_slot('quiz_question_list')
+        question = quiz_question_list[quiz_question_count][0]
+        
+        # fetch the options for the questions
+        options = quiz_question_list[quiz_question_count][1:][0]
+        dispatcher.utter_message(text=show_question_options(question, options))
+
+        return [SlotSet('quiz_question_count', quiz_question_count)]
+
+# This class is to called at the end of the quiz. It will show the final score and add the scoe in DB
+class QuizFormSubmit(Action):
+    def name(self) -> Text:
+        return "action_quiz_form_submit"
+    
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: DomainDict
+    ) -> List[Dict[Text, Any]]:
+        
+        quiz_number = tracker.get_slot('quiz_number')
+        quiz_correct_ans = tracker.get_slot('quiz_correct_ans')
+        quiz_question_count = tracker.get_slot('quiz_question_count')
+        email = tracker.get_slot('email')
+        if email is not None:
+            ask = f"Quiz Completed !!\nYour score is {quiz_correct_ans}/{quiz_question_count}"
+            dispatcher.utter_message(text=ask)
+            score = 100.0*quiz_correct_ans/quiz_question_count
+            insertScore(score, quiz_number, email)
+        else:
+            dispatcher.utter_message(text="Thanks for taking the quiz. Please register yourself to see your score")
+
+        return [SlotSet("quiz_number", None),
+                SlotSet('quiz_question_list', None),
+                SlotSet('quiz_original_question_list', None),
+                SlotSet('quiz_over', None),
+                SlotSet('quiz_correct_ans', 0),
+                SlotSet('quiz_question_count', 0)]
+
+class QuizShowScore(Action):
+    def name(self) -> Text:
+        return "action_quiz_show_score"
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: DomainDict
+    ) -> List[Dict[Text, Any]]:
+
+        email = tracker.get_slot('email')
+        if email is not None:
+            s = getScore(email)
+            dispatcher.utter_message(text=s)
+        else:
+            dispatcher.utter_message(text="Please login to see your performance")
+
+
+
+            # This form is to launch quiz for a user
+# 'class QuizForm(Action):
+#     def name(self) -> Text:
+#         return "action_quiz_form"'
+    
+    # def submit(
+    #         self,
+    #         dispatcher: CollectingDispatcher,
+    #         tracker: Tracker,
+    #         domain: Dict[Text, Any],
+    # ) -> List[Dict]:
+    #     """Define what the form has to do
+    #         after all required slots are filled"""
+    #     # utter submit template
+    #     quiz_course = tracker.get_slot('quiz_course')
+    #     quiz_number = tracker.get_slot('quiz_number')
+    #     quiz_over = tracker.get_slot('quiz_over')
+    #     answer = tracker.get_slot('answer')
+    #     logger.info(f"[{tracker.sender_id}] {__file__} :  Inside quiz_info_form submit function called ")
+    #     logger.info(f"[{tracker.sender_id}] {__file__} :  {quiz_course} {quiz_number} {quiz_over} {answer}")
+    #     # dispatcher.utter_message("Thank you for providing the required information!")
+    #     return []
+    
+    # def run(
+    #     self, 
+    #     dispatcher: CollectingDispatcher, 
+    #     tracker: Tracker, 
+    #     domain: Dict
+    # ) -> List[EventType]:
+        
+    #     logger.info(f"[{tracker.sender_id}] {__file__}")
+
+    #     required_slots = ["quiz_number", "quiz_over"]
+
+    #     quiz_number = tracker.slots.get("quiz_number")
+    #     logger.info(f"quiz_number: {quiz_number}")
+
+    #     for slot_name in required_slots:
+    #         if tracker.slots.get(slot_name) is None:
+    #             if slot_name == 'quiz_number':
+                    
+    #             elif slot_name == 'quiz_over':
+    #                 tell = "Please enter the quiz_over that you want to launch ?"
+    #                 dispatcher.utter_message(text=tell)
+    #             return [SlotSet("requested_slot", slot_name)]
+
+    #         dispatcher.utter_message(text="H: " + str(tracker.slots.get(slot_name)) + " slot_name " + slot_name)
+    #     tell = "Please enter the quiz number that you want to launch ?"
+    #     dispatcher.utter_message(text=tell)
+
+    #     return [SlotSet("requested_slot", None)]
 
 # This class is to validates the user input given
 # class ValidateQuizForm(FormValidationAction):
@@ -176,103 +284,3 @@ class QuizForm(Action):
 #         # increment the question number
 #         quiz_question_count = quiz_question_count + 1
 #         return {"quiz_over": quiz_over, "quiz_question_count": quiz_question_count, 'quiz_correct_ans': quiz_correct_ans}
-
-# This class will ask the user to select the quiz that he/she wants to take for form quiz_info_form
-class QuizNumber(Action):
-
-    def name(self) -> Text:
-        return "action_ask_quiz_number"
-    def run(
-        self, 
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> Dict[Text, Any]:
-
-        course_id = current_course_id(tracker)
-        listofQuiz = getListofQuiz(course_id)
-        if len(listofQuiz) == 0:
-            ask = f"There is no quiz for course {tracker.get_slot('quiz_course')}"
-            dispatcher.utter_message(text=ask)
-        else:
-            ask = f"Please enter the quiz number from {listofQuiz}"
-            ask = f"Please note that your answers must be separated by a coma"
-            buttons = quiz_buttons(listofQuiz)
-            buttons = button_it(buttons)
-            dispatcher.utter_message(text=ask, buttons=buttons)
-        return []
-
-# action_ask_<slot>
-# This class will ask the quiz question to the user for form quiz_info_form
-class QuizOver(Action):
-    def name(self) -> Text:
-        return "action_ask_quiz_over"
-
-    def run(self, 
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]
-    ) -> List[Dict[Text, Any]]:
-        
-        # get question number that needs to be asked
-        quiz_question_count = tracker.get_slot('quiz_question_count')
-        if quiz_question_count is None:
-            quiz_question_count = 0
-       
-        # fetch the question from the list of question
-        quiz_question_list = tracker.get_slot('quiz_question_list')
-        question = quiz_question_list[quiz_question_count][0]
-        
-        # fetch the options for the questions
-        options = quiz_question_list[quiz_question_count][1:][0]
-        dispatcher.utter_message(text=show_question_options(question, options))
-
-        return [SlotSet('quiz_question_count', quiz_question_count)]
-
-# This class is to called at the end of the quiz. It will show the final score and add the scoe in DB
-class QuizFormSubmit(Action):
-    def name(self) -> Text:
-        return "action_quiz_form_submit"
-    
-    def run(
-        self,
-        dispatcher,
-        tracker: Tracker,
-        domain: DomainDict
-    ) -> List[Dict[Text, Any]]:
-        
-        quiz_number = tracker.get_slot('quiz_number')
-        quiz_correct_ans = tracker.get_slot('quiz_correct_ans')
-        quiz_question_count = tracker.get_slot('quiz_question_count')
-        email = tracker.get_slot('email')
-        if email is not None:
-            ask = f"Quiz Completed !!\nYour score is {quiz_correct_ans}/{quiz_question_count}"
-            dispatcher.utter_message(text=ask)
-            score = 100.0*quiz_correct_ans/quiz_question_count
-            DB.insertScore(score, quiz_number, email)
-        else:
-            dispatcher.utter_message(text="Thanks for taking the quiz. Please register yourself to see your score")
-
-        return [SlotSet("quiz_number", None),
-                SlotSet('quiz_question_list', None),
-                SlotSet('quiz_original_question_list', None),
-                SlotSet('quiz_over', None),
-                SlotSet('quiz_correct_ans', 0),
-                SlotSet('quiz_question_count', 0)]
-
-class QuizShowScore(Action):
-    def name(self) -> Text:
-        return "action_quiz_show_score"
-    def run(
-        self,
-        dispatcher,
-        tracker: Tracker,
-        domain: DomainDict
-    ) -> List[Dict[Text, Any]]:
-
-        email = tracker.get_slot('email')
-        if email is not None:
-            s = getScore(email)
-            dispatcher.utter_message(text=s)
-        else:
-            dispatcher.utter_message(text="Please login to see your performance")
